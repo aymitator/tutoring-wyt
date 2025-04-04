bb3ae0f41e12c06ba0535f929486013e83c5d485
aymitator <hollawac@outlook.com>
2025-04-04 15:04:59 -0700

    Add files via upload

---
+++ liarliar.py
@@ -0,0 +1,31 @@
+
+#COREY LIES TO HIS OWN-OWN
+
+import sys
+import time
+
+def writeln(_str):
+    print(_str)
+def cin(_str):
+    return input(_str + ' ')
+def zit():
+    sys.exit(0)
+def slee(_n):
+    time.sleep(_n)
+
+english = cin("What are you scared a'?")
+arenglish = english.split(' ')
+for i in arenglish:
+    szsz = i.lower()
+    if 'steal' in szsz:
+        writeln('redeemed')
+    elif 'lying' in szsz:
+        writeln('rebranding')
+    elif 'unorganized' in szsz:
+        writeln('being das bottle-neck')
+    elif 'tres' in szsz:
+        writeln('being looted')
+
+    slee(6)
+
+zit()
